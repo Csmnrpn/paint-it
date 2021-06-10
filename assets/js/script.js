7a05d6a5cc9e30ba1531586507e3b5a23f8c03c1
Cosmin R <cosminripan33@gmail.com>
2021-06-10 21:28:04 +0300

    grid change is done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,53 @@
 const canvasContainer = document.querySelector('.canvasContainer');
 const clearButton = document.querySelector('.clearButton');
+const sizeButton = document.querySelector('.sizeButton');
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('canvasSquares');
-    canvasContainer.appendChild(div);
+let size = 10;
+
+function createCanvas(size) {
+    canvasContainer.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+    for (let i = 0; i < size*size; i++) {
+        let div = document.createElement('div');
+        div.classList.add('canvasSquares');
+        canvasContainer.appendChild(div);
+    }
+    
 }
 
-const canvasSquares = document.querySelectorAll('.canvasSquares');
-canvasSquares.forEach(square => {
-    square.addEventListener ('mouseover', function(){
-        square.classList.toggle('red');
-})});
+createCanvas(size);
+
+let canvasSquares = document.querySelectorAll('.canvasSquares');
+
+function canvasRed() {
+    canvasSquares.forEach(square => {
+        square.addEventListener ('mouseover', function(){
+            square.classList.add('red');
+        })});
+}
+
+canvasRed();
+
+sizeButton.addEventListener('click', function() {
+    size = Number(prompt("Please enter the size of the canvas. (max: 100px)"));
+
+    canvasSquares.forEach(square => {
+        square.remove();
+    })
+    createCanvas(size);
+
+    canvasSquares = document.querySelectorAll('.canvasSquares');
+
+    canvasRed();
+})
+
 
 clearButton.addEventListener('click', function(){
     canvasSquares.forEach(square => {
         square.classList.remove('red');
     })
-})
+});
+
+
+
+
+
